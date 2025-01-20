a59861c7e7c22700f62a186a83daca28204a6558
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-01-20 21:05:27 +0330

    feat: update backup files

--- assets/postgres-backup/init.sh
+++ assets/postgres-backup/init.sh
@@ -248,7 +248,7 @@ set "PGPASSWORD=postgres"
 PGPASSWORD="postgres"
 pg_restore -h localhost -p 5432 -U postgres -d $authDatabaseName -v "$authDatabaseName.bak";
 pg_restore -h localhost -p 5432 -U postgres -d $integrationDatabaseName -v "$integrationDatabaseName.bak";
-pg_restore -h localhost -p 5432 -U postgres -d $integrationTypesDatabaseName -v "$integrationTypesDatabaseName.bak";
+# pg_restore -h localhost -p 5432 -U postgres -d $integrationTypesDatabaseName -v "$integrationTypesDatabaseName.bak";
 pg_restore -h localhost -p 5432 -U postgres -d $complianceDatabaseName -v "$complianceDatabaseName.bak";
 pg_restore -h localhost -p 5432 -U postgres -d $dexDatabaseName -v "$dexDatabaseName.bak";
 pg_restore -h localhost -p 5432 -U postgres -d $coreDatabaseName -v "$coreDatabaseName.bak";
