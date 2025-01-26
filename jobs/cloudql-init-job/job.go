7fe80b447d39319e0a789962a962ae073dfa5023
artaasadi <arta-c@kaytu.io>
2025-01-26 13:07:26 +0100

    fix: optimize memory usage on cloudql init

--- jobs/cloudql-init-job/job.go
+++ jobs/cloudql-init-job/job.go
@@ -84,8 +84,8 @@ func (j *Job) Run(ctx context.Context) error {
 				}
 			}
 
-			var pluginBinary models.IntegrationPluginBinary
-			err = db.Model(models.IntegrationPluginBinary{}).Where("plugin_id = ?", plugin.PluginID).Find(&pluginBinary).Error
+			var cloudqlBinary []byte
+			err = db.Raw("SELECT cloud_ql_plugin FROM integration_plugin_binary WHERE plugin_id = ?", plugin.PluginID).Scan(&cloudqlBinary).Error
 			if err != nil {
 				j.logger.Error("failed to get plugin binary", zap.Error(err), zap.String("plugin_id", plugin.PluginID))
 				return err
@@ -93,11 +93,13 @@ func (j *Job) Run(ctx context.Context) error {
 
 			// write the plugin to the file system
 			pluginPath := dirPath + "/steampipe-plugin-" + describerConfig.SteampipePluginName + ".plugin"
-			err := os.WriteFile(pluginPath, pluginBinary.CloudQlPlugin, 0777)
+			err := os.WriteFile(pluginPath, cloudqlBinary, 0777)
 			if err != nil {
 				j.logger.Error("failed to write plugin to file system", zap.Error(err), zap.String("plugin", describerConfig.SteampipePluginName))
 				return err
 			}
+
+			cloudqlBinary = nil
 		}
 	}
 	if err := steampipe.PopulateOpenGovernancePluginSteampipeConfig(j.cfg.ElasticSearch, j.cfg.Steampipe); err != nil {
