71167824b305d4ff14e204aa01b787e7dec66731
artaasadi <arta-c@kaytu.io>
2025-04-02 18:16:19 +0200

    fix: fixed packages column in artifact sbom

--- pkg/cloudql/client/artifact_sbom.go
+++ pkg/cloudql/client/artifact_sbom.go
@@ -18,6 +18,7 @@ const (
 type ArtifactSbom struct {
 	ImageURL          string      `json:"image_url"`
 	ArtifactID        string      `json:"artifact_id"`
+	Packages          []string    `json:"packages"`
 	SbomSpdxJson      interface{} `json:"sbom_spdx_json"`
 	SbomCyclonedxJson interface{} `json:"sbom_cyclonedx_json"`
 }
