f3a48f125678d1f5ab9e0220c034fb28cdc3497b
Arta Asadi <artaasadi@Artas-MacBook-Pro.local>
2025-04-16 20:33:13 +0200

    fix: update export plugin data script

--- scripts/export_plugin_data.sh
+++ scripts/export_plugin_data.sh
@@ -1,6 +1,78 @@
+echo "NEEDED ENV Variables are: [PLUGIN_ID, ES_INDEX_PREFIX, DB_PASSWORD, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, ENDPOINT_URL, ELASTICSEARCH_PASSWORD, OPENSSL_PASSWORD, DEMO_DATA_S3_PATH]"
+
 mkdir -p /tmp/demo-data
 
+
+DB_HOST="${DB_HOST:-localhost}"
+DB_PORT="${DB_PORT:-5432}"
+DB_USER="${DB_USER:-postgres}"
+DB_PASSWORD="${DB_PASSWORD}" # Should be set in the environment
+DB_NAME="${DB_NAME:-integration}"
+OUTPUT_FILE="/tmp/demo-data/integrations.json"
+
+if [ -z "$DB_PASSWORD" ]; then
+    echo "Error: DB_PASSWORD environment variable is not set." >&2
+    exit 1
+fi
+
+if [ -z "$PLUGIN_ID" ]; then
+    echo "Error: PLUGIN_ID environment variable is not set." >&2
+    exit 1
+fi
+
+if [ -z "$ES_INDEX_PREFIX" ]; then
+    echo "Error: ES_INDEX_PREFIX environment variable is not set." >&2
+    exit 1
+fi
+
+export PGPASSWORD="$DB_PASSWORD"
+
+SQL_QUERY=$(cat <<-EOF
+SELECT
+    json_build_object(
+        'integrationId', integration_id::text, -- Cast UUID to text if needed
+        'providerId', provider_id,
+        'name', name,
+        'integrationType', integration_type, -- Assuming it's stored as text compatible with your Go type
+        'annotations', annotations,       -- Assuming annotations is a JSONB column
+        'labels', labels              -- Assuming labels is a JSONB column
+    )
+FROM
+    integrations
+WHERE
+    integration_type = '${PLUGIN_ID}';
+EOF
+)
+
+echo "Connecting to database '$DB_NAME' on '$DB_HOST:$DB_PORT' as user '$DB_USER'..."
+echo "Executing query and exporting to '$OUTPUT_FILE'..."
+
+psql -X -A -t -q -v ON_ERROR_STOP=1 \
+    -h "$DB_HOST" \
+    -p "$DB_PORT" \
+    -U "$DB_USER" \
+    -d "$DB_NAME" \
+    -c "$SQL_QUERY" | \
+jq -s '.' > "$OUTPUT_FILE"
+
+if [ -s "$OUTPUT_FILE" ]; then
+    echo "Successfully exported integrations to '$OUTPUT_FILE'."
+else
+    echo "Error: Failed to export data or the table is empty. Output file is empty or not created." >&2
+    exit 1
+fi
+
+unset PGPASSWORD
+
+echo "Starting dumping elasticsearch data"
+
 mkdir -p /tmp/demo-data/es-demo
+ELASTICSEARCH_USERNAME="${ELASTICSEARCH_USERNAME:-admin}"
+ELASTICSEARCH_ADDRESS="${ELASTICSEARCH_ADDRESS:-https://localhost:9200}"
+if [ -z "$ELASTICSEARCH_PASSWORD" ]; then
+    echo "Error: ELASTICSEARCH_PASSWORD environment variable is not set." >&2
+    exit 1
+fi
 NEW_ELASTICSEARCH_ADDRESS="https://${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}@${ELASTICSEARCH_ADDRESS#https://}"
 mkdir -p ~/.config/rclone
 
@@ -22,7 +94,7 @@ multielasticdump \
   --input="$NEW_ELASTICSEARCH_ADDRESS" \
   --output="/tmp/demo-data/es-demo/" \
   --parallel=20 \
-  --match='^(?!\.)(?!.*(security|deleted|logs|metrics)).*$' \
+  --match='^'${ES_INDEX_PREFIX}'.*$' \
   --matchType=alias \
   --limit=10000 \
   --scrollTime=10m \
